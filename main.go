f28f1ca614eb2631c42bcb5f4cbed31eca0ecdae
Niklas Fasching <niklas.fasching@gmail.com>
2019-01-09 23:25:13 +0100

    Refactor command line interface & support render to html+pdf

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"sync"
@@ -49,6 +49,8 @@ type Resume struct {
 	Recommendations []string
 }
 
+var l = log.New(os.Stderr, "", 0)
+
 var autoReloadScriptTag = `
 <script>
   fetch(location.pathname, {method: 'POST'}).then(() => location.reload());
@@ -82,20 +84,66 @@ var errorTemplate = template.Must(template.New("error").Parse(`
   {{ . }}
 </html>`))
 
+var ChromeExecutableNames = []string{
+	"chromium-browser",
+	"google-chrome",
+}
+
 func main() {
-	if len(os.Args) < 2 || len(os.Args) > 3 {
-		log.Println("USAGE: 'resume RESUME_DATA_FILE' to start a server")
-		log.Fatal("USAGE: 'resume RESUME_DATA_FILE TEMPLATE_FILE' to render HTML to stdout")
+	if len(os.Args) == 1 {
+		printUsageAndExit()
 	}
-
-	if len(os.Args) == 3 {
-		html := renderTemplate(os.Args[2], os.Args[1])
-		fmt.Fprint(os.Stdout, html)
-		return
+	switch cmd := os.Args[1]; cmd {
+	case "server":
+		if len(os.Args) != 3 {
+			printUsageAndExit()
+		}
+		templatesRoot, resumePath := "templates/", os.Args[2]
+		serve(templatesRoot, resumePath)
+	case "render":
+		if len(os.Args) != 4 {
+			printUsageAndExit()
+		}
+		resumePath, templatePath := os.Args[2], os.Args[3]
+		render(templatePath, resumePath)
+	default:
+		printUsageAndExit()
 	}
+}
 
-	resumePath, templatesRoot := os.Args[1], "templates/"
+func render(templatePath, resumePath string) {
+	html, err := renderTemplate(templatePath, resumePath)
+	if err != nil {
+		l.Fatalf("Could not render template: %s", err)
+	}
+	err = ioutil.WriteFile("resume.html", []byte(html), 0644)
+	if err != nil {
+		l.Fatalf("Could not write resume.html: %s", err)
+	}
+	chromePath := chromeExecutable()
+	if chromePath == "" {
+		l.Println("Skip resume.pdf rendering: chrome not found & CHROME_EXECUTABLE env var not set")
+		os.Exit(0)
+	}
+	cmd := exec.Command(chromePath,
+		"--headless",
+		"--disable-gpu",
+		"--virtual-time-budget=10000",
+		"--print-to-pdf=resume.pdf",
+		"resume.html")
+	err = cmd.Run()
+	if err != nil {
+		l.Fatal("Could not render resume.pdf:", err)
+	}
+}
 
+func printUsageAndExit() {
+	l.Println("USAGE: 'resume server RESUME_ORG_PATH': Start server on :8000 and render resume templates")
+	l.Println("USAGE: 'resume render RESUME_ORG_PATH TEMPLATE_PATH': Render resume.html & resume.pdf in current directory")
+	os.Exit(0)
+}
+
+func serve(templatesRoot, resumePath string) {
 	// a crude version of auto-reload is implemented via long polling:
 	// - on load a HTTP POST is fired off. On successful completion of the request the page reloads
 	// - the server keeps all POST requests open
@@ -110,7 +158,7 @@ func main() {
 		pendingMutex.Unlock()
 	})
 	if err != nil {
-		log.Fatal(err)
+		l.Fatal(err)
 	}
 	defer watcher.Close()
 
@@ -123,14 +171,14 @@ func main() {
 			pendingMutex.Unlock()
 			<-c
 		case req.URL.Path == "/":
-			w.Write(htmlWithAutoReloadTag(renderIndex(templatesRoot)))
+			w.Write(renderWithAutoReload(renderIndex(templatesRoot)))
 		default:
 			templatePath := filepath.Join(templatesRoot, req.URL.Path)
-			w.Write(htmlWithAutoReloadTag(renderTemplate(templatePath, resumePath)))
+			w.Write(renderWithAutoReload(renderTemplate(templatePath, resumePath)))
 		}
 	})
-	log.Println("Listening on :8000")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	l.Println("Listening on :8000")
+	l.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 func watch(paths []string, f func(path string)) (*fsnotify.Watcher, error) {
@@ -147,7 +195,7 @@ func watch(paths []string, f func(path string)) (*fsnotify.Watcher, error) {
 				}
 			case err, ok := <-watcher.Errors:
 				if ok {
-					log.Println("error:", err)
+					l.Println("error:", err)
 				}
 			}
 		}
@@ -161,51 +209,62 @@ func watch(paths []string, f func(path string)) (*fsnotify.Watcher, error) {
 	return watcher, nil
 }
 
-// render errors as html rather than using http.Error() so we have auto-reload
-func renderError(err error) string {
-	w := strings.Builder{}
-	err = errorTemplate.Execute(&w, err)
-	if err != nil {
-		return err.Error()
-	}
-	return w.String()
-}
-
-func renderIndex(templatesRoot string) string {
+func renderIndex(templatesRoot string) (string, error) {
 	fs, err := ioutil.ReadDir(templatesRoot)
 	if err != nil {
-		return renderError(err)
+		return "", err
 	}
 	w := strings.Builder{}
 	err = indexTemplate.Execute(&w, fs)
 	if err != nil {
-		return renderError(err)
+		return "", err
 	}
-	return w.String()
+	return w.String(), nil
 }
 
-func renderTemplate(templatePath, resumePath string) string {
+func renderTemplate(templatePath, resumePath string) (string, error) {
 	bs, err := ioutil.ReadFile(resumePath)
 	if err != nil {
-		return renderError(err)
+		return "", err
 	}
 	resume := Resume{}
 	err = orgiaml.New().Unmarshal(bytes.NewReader(bs), resumePath, &resume)
 	if err != nil {
-		return renderError(err)
+		return "", err
 	}
 	t, err := template.ParseFiles(templatePath)
 	if err != nil {
-		return renderError(err)
+		return "", err
 	}
 	w := strings.Builder{}
 	err = t.Execute(&w, resume)
 	if err != nil {
-		return renderError(err)
+		return "", err
 	}
-	return w.String()
+	return w.String(), nil
 }
 
-func htmlWithAutoReloadTag(html string) []byte {
-	return []byte(strings.Replace(html, "</html>", autoReloadScriptTag+"</html>", 1))
+func renderWithAutoReload(html string, err error) []byte {
+	if err != nil {
+		w := strings.Builder{}
+		err = errorTemplate.Execute(&w, err)
+		if err != nil {
+			html = "<!DOCTYPE html><html><body>" + err.Error() + "</body></html>"
+		} else {
+			html = w.String()
+		}
+	}
+	return []byte(strings.Replace(html, "</html>", autoReloadScriptTag+"</html>", -1))
+}
+
+func chromeExecutable() string {
+	if path := os.Getenv("CHROME_EXECUTABLE"); path != "" {
+		return path
+	}
+	for _, name := range ChromeExecutableNames {
+		if path, err := exec.LookPath(name); err == nil {
+			return path
+		}
+	}
+	return ""
 }
